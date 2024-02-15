5ff66c68678b05daececeae633f4e63afdbb7419
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-02-15 08:29:48 +0200

    Style the Footer for different breakpoints

--- countdown-timer/src/Footer.jsx
+++ countdown-timer/src/Footer.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import hillsImage from './images/pattern-hills.svg';
+import hillsImage from "./images/pattern-hills.svg";
 
 let styles = {
-    backgroundImage: `url(${hillsImage})`
-}
+  backgroundImage: `url(${hillsImage})`,
+};
 
 export const Footer = () => {
   return (
-    <div className='w-full min-h-[150px] bg-cover bg-right-bottom absolute bottom-0 left-0 flex justify-center items-center md:bg-top' style={styles}>
+    <div className='w-full min-h-[150px] bg-cover bg-right-bottom absolute bottom-0 left-0 flex justify-center items-center sm:min-h-[100px] lg:min-h-[150px] md:bg-top' style={styles}>
         <div className='flex flex-row w-64 justify-between'>
             <i className="fa-brands fa-facebook text-[36px] text-slate-400 hover:text-[#fb6087] duration-300"></i>
             <i className="fa-brands fa-pinterest text-[36px] text-slate-400 hover:text-[#fb6087] duration-300"></i>
@@ -16,4 +16,4 @@ export const Footer = () => {
         </div>
     </div>
   )
-}
+};
